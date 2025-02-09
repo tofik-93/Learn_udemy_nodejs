4cf5588b3a6af1c8087ec45228474f1a0e9ee177
tofik-93 <tofikmifta87@gmail.com>
2025-02-09 14:28:51 +0300

    upadate the routhin js

--- routes/Routhing.js
+++ routes/Routhing.js
@@ -1,5 +1,19 @@
+const express = require('express')
+const app = express()
+
+
+
+app.use ((req,res, next) =>{
+    console.log(`request URL: ${req.url}`)
+    next()
+})
+
+//Router handler 
+app.get ('/', (req, res)=>{
+res.send('hello world')})
+
 app.use('/api',(req, res, next)=>{
-    next();
+    next();// pass control to the next middleware or route handler
 })  // valid path values for mounting middelware
 app.use('/api(c?)d', (req, res, next)=>{
     next()})// path pattern
@@ -12,3 +26,16 @@ app.use('/api(c?)d', (req, res, next)=>{
 app.use(['/abcd','xyza', /\/lmn | \/pqr/], (req,res, next)=>{
     next()
 })
+
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000')
+})
+
+
+//*********Middle callback fuction example */
+const router = express.Router()
+router.get('/', (req, res, next)=>{
+    next()
+})
+
+app.use(router)
