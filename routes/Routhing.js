dbb10db5b82b25d0649bc490ea045747adee1d9d
tofik-93 <tofikmifta87@gmail.com>
2025-02-10 08:36:44 +0300

    update route

--- routes/Routhing.js
+++ routes/Routhing.js
@@ -3,19 +3,21 @@ const app = express()
 
 
 
-app.use ((req,res, next) =>{
-    console.log(`request URL: ${req.url}`)
-    next()
-})
+// app.use ((req,res, next) =>{
+//     console.log(`request URL: ${req.url}`)
+//     next()
+// })
 
 //Router handler 
 app.get ('/', (req, res)=>{
 res.send('hello world')})
 
 app.use('/api',(req, res, next)=>{
+     console.log('api middleware')
     next();// pass control to the next middleware or route handler
 })  // valid path values for mounting middelware
 app.use('/api(c?)d', (req, res, next)=>{
+    console.log('checked')
     next()})// path pattern
     
     app.use ('/\/api |\/xyz/', (req, res, next)=>{
@@ -24,6 +26,7 @@ app.use('/api(c?)d', (req, res, next)=>{
 
 
 app.use(['/abcd','xyza', /\/lmn | \/pqr/], (req,res, next)=>{
+  console.log('multiple path')
     next()
 })
 
