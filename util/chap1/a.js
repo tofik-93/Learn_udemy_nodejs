25415e27c5ecced32840a60cc2268f01a9cafd86
tofik-93 <tofikmifta87@gmail.com>
2025-03-04 23:59:15 +0300

    dn

--- util/chap1/a.js
+++ util/chap1/a.js
@@ -2,10 +2,21 @@
 // const b = require('./b')
 // exports.loaded = true
 // file logger.js
-exports.info = (message)=> {
+exports.info = (message)=> { // exporting the module
     console.log(`info:${message}`)
 }
 exports.verbose = (message) => {
     console.log(`verbose:${message}`)
 }
 
+// utils.js
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+function farewell(name) {
+    return `Goodbye, ${name}!`;
+}
+
+// Exporting multiple functions as an object
+module.exports = { greet, farewell };
